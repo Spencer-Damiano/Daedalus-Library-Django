29c034f5e0f912d80f988db768182b3f3adbc5e3
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2023-10-10 23:36:57 -0600

    Stopwatch works, lots of bugs
    Known bugs at breakTimer - doesn't count down or show that the values are decreasing
    finished - doesn't finish anything. kinda of confusing

--- static/src/StudySession.ts
+++ static/src/StudySession.ts
@@ -1,20 +1,85 @@
 
-export class StudySession {
-    private ttlStudyTime: number;
-    private ttlBreakTime: number;
+let studyBol = false;
+let breakBol = false;
 
-    constructor() {
-        this.ttlStudyTime = 0;
-        this.ttlBreakTime = 0;
+
+export function flowTimer(startTime: number): number {
+    studyBol = true;
+    let count = 0;
+
+    if (studyBol) {
+        console.log('Start Time: ' + startTime)
+        count = (Date.now() - startTime) / 1000;
+        _formatTime(count);
     }
+    studyBol = false;
+    return count;
+}
 
-    study(): number {
-        return this.ttlStudyTime;
+export function breakTimer(endTime: number) {
+    breakBol = true;
+    let count = 0;
+
+    if (breakBol) {
+        let currentTime: number = Date.now();
+
+        count = (endTime - currentTime) / 1000;
+        console.log('count: ' + count);
+        _formatTime(count);
+
+        if (count <= 0) {
+            breakBol = false;
+            console.log('Break Time is up');
+        }
     }
+    breakBol = false;
+    return count;
+}
 
-    break(breakTime:number): void {
-        breakTime = breakTime / 5;
-        this.ttlBreakTime += breakTime;
-        console.log(this.ttlBreakTime);
+
+export function finished() {
+    studyBol = false;
+    breakBol = false;
+    console.log('Finished');
+}
+
+function _formatTime(count: number): void {
+    const timer = document.getElementById("timer");
+
+    if (timer &&  studyBol) {
+        if (count <= 60){
+            let hrs = (0).toString().padStart(2, '0');
+            let mins = (0).toString().padStart(2, '0');
+            let secs = Math.floor(count).toString().padStart(2, '0');
+
+            timer.innerHTML = `${hrs}:${mins}:${secs}`;
+        } else if (count > 60 && count <= 3600) {
+            let hrs = (0).toString().padStart(2, '0');
+            let mins = Math.floor(count / 60).toString().padStart(2, '0');
+            let secs = Math.floor(count % 60).toString().padStart(2, '0');
+
+            timer.innerHTML = `${hrs}:${mins}:${secs}`;
+        } else if (count > 3600) {
+            let hrs = Math.floor(count / 3600).toString().padStart(2, '0');
+            let mins = Math.floor((count % 3600) / 60).toString().padStart(2, '0');
+            let secs = Math.floor(count % 60).toString().padStart(2, '0');
+
+            timer.innerHTML = `${hrs}:${mins}:${secs}`;
+        } else {
+            let hrs = (-99).toString().padStart(2, '0');
+            let mins = (-99).toString().padStart(2, '0');
+            let secs = (-99).toString().padStart(2, '0');
+            
+            timer.innerHTML = `${hrs}:${mins}:${secs}`;
+            console.log("Error in formatTime function");
+            
+        }
+    } else if (timer && breakBol){
+        const hrs = Math.floor(count / 3600000).toString().padStart(2, '0') ;
+        const mins = Math.floor((count % 3600000) / 60000).toString().padStart(2, '0');
+        const secs = Math.floor((count % 60000) / 1000).toString().padStart(2, '0'); 
+        
+        
+        timer.innerHTML = `${hrs}:${mins}:${secs}`;
     }
 }
--- static/src/script.ts
+++ static/src/script.ts
@@ -1,5 +1,5 @@
 // Import other functions
-import { StudySession } from "./StudySession.js";
+import { flowTimer, breakTimer, finished } from "./StudySession.js";
 
 // Document Elements
 
@@ -9,21 +9,61 @@ const studyBtn = document.getElementById("studyBtn");
 const breakBtn = document.getElementById("breakBtn");
 const finishedBtn = document.getElementById("finishedBtn");
 
+
+
+
 // Classes and Variables
-let _studySession = new StudySession();
+let studyBol = false;
+let breakBol = false;
+
+let studyingTime = 0;
 
 
 // Event Listeners
 
 if (studyBtn) {
   studyBtn.addEventListener("click", () => {
-    console.log(_studySession.study());
+    console.log("study button clicked");
+    breakBol = false;
+    studyBol = true;
+
+    
+    let startTime = Date.now();
+
+    if (studyBol) {
+      setInterval(() => {studyingTime = flowTimer(startTime)});
+    }
   });
 }
 
 if (breakBtn) {
   breakBtn.addEventListener("click", () => {
-    _studySession.break(0);
+    console.log("break button clicked");
+    studyBol = false;
+    breakBol = true;
+
+    // studyingTime = Date.now() + 10000; // this is for testing purposes, just hit the break button if enabled
+
+    let breakTime = Date.now() + (studyingTime / 5);
+
+    if (breakBol) {
+      setInterval(() => {breakTimer(breakTime)});
+    }
   });
 }
+
+
+if (finishedBtn) {
+  finishedBtn.addEventListener("click", () => {
+    console.log("finished button clicked");
+    finished();
+
+    studyBol = false;
+    breakBol = false;
+    
+  });
+}
+
+
+// Functions
 
